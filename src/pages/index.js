9df77bd1ca9442d043792f461f5ed9c0e6715075
ArcanisCz <arcaniscz@gmail.com>
2019-11-02 15:30:54 +0100

    build fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,27 +1,10 @@
 import React from "react";
-import {graphql} from "gatsby";
 
 import {Seo} from "components";
 
-/* eslint-disable */
-export default ({data}) => (
+export default () => (
     <>
         <Seo title="" />
-        {console.log(data)}
+        aaaaaaa
     </>
 );
-
-export const query = graphql`
-query {
-  news: allNewsYaml(sort: {fields: [date], order: ASC}) {
-    edges {
-      node {
-        id
-        title
-        date
-        content
-      }
-    }
-  }  
-}
-`;
